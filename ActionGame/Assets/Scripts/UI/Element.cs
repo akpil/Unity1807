53391e642da3e5f73119c82dafbe4fb1b8d4ab0c
akpil <jyk0802@naver.com>
2018-08-24 13:57:19 +0900

    Lobby work start

---
+++ ActionGame/Assets/Scripts/UI/Element.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Element : MonoBehaviour {
+
+    [SerializeField]
+    private Image icon;
+    [SerializeField]
+    private Text TitleText,
+                 CostText,
+                 Contents;
+    [SerializeField]
+    private Button PurchaseButton;
+
+	// Use this for initialization
+	void Start () {
+        PurchaseButton.onClick.AddListener(TextFunction);
+
+    }
+
+    public void TextFunction()
+    {
+        Debug.Log("Button Click Test");
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
